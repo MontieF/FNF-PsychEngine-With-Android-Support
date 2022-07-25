38999b4973ae8f8b823b8fa289361d46c8f3d96b
MontieF <106031579+MontieF@users.noreply.github.com>
2022-07-24 23:20:32 -0400

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3023,8 +3023,8 @@ class PlayState extends MusicBeatState
 		iconP1.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01)) + (150 * iconP1.scale.x - 150) / 2 - iconOffset;
 		iconP2.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01)) - (150 * iconP2.scale.x) / 2 - iconOffset * 2;
 
-		if (health > 2)
-			health = 2;
+		if (health > 3)
+			health = 3;
 
 		if (healthBar.percent < 20)
 			iconP1.animation.curAnim.curFrame = 1;
